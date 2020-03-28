c912a4c4716987f7c5b7c1e1bf65cadd9b4ebdee
svd246 <svd246@gmail.com>
2020-03-28 00:03:41 -0400

    everything works

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 '''
 Samip Vaidh
 3/31/20
-Knights Tour
+Lexicographic Tree
 '''
 
 #create a hash map for getting the number of lines and words from the input file
@@ -9,6 +9,7 @@ Knights Tour
 words_hash = {}
 #opening the file and assigning it to the read word variable and once it is finised the variable gets disposed
 with open("lexi.txt", "r") as read_word:
+    line_number = 1
     #looping through the file starting at the first line to take each word from the file
     for line in read_word:
         #create a variable to store the appened letters and spaces that are created by the loop
@@ -33,14 +34,13 @@ with open("lexi.txt", "r") as read_word:
             short_word = word[:10] 
             #finds the key if the word has been seen before and adds a value of 1
             if short_word in words_hash.keys():
-                words_hash[short_word] += 1
+                if line_number not in words_hash[short_word]:
+                    words_hash[short_word].append(line_number)
             #if the key is not found then it will assign that word as new a new key and map that to the value 1
             else:
-                words_hash[short_word] = 1
-
-#print the key and values for each word in the map
-for key in words_hash.keys():
-    print ('{}: {}'.format(key, words_hash[key]))
+                words_hash[short_word] = [line_number]
+        #line number counter goes up
+        line_number += 1
 
 #place words into a binary tree
 #create a node for the BST
@@ -67,9 +67,22 @@ def insert(root, node):
                 insert(root.left, node)
 
 #traverse the tree in order
-def in_order(root):
-    if root:
-        in_order(root.left)
+def print_in_order(root):
+    if root is not None:
+        print_in_order(root.left)
         print(root.val)
-        in_order(root.right)
+        print_in_order(root.right)
 
+#printing the hash map and tree
+root = None
+print("\n**Word count of file**\n")
+for key in words_hash.keys():
+    print('{}: {}'.format(key, words_hash[key]))
+    if root is None:
+        root = Node(key)
+    else:
+        new_node = Node(key)
+        insert(root, new_node)
+
+print("\n**In order traversal of BST**\n")
+print_in_order(root)
